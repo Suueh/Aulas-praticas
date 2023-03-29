ca3165e135363f007e596ad40ab8453535ccabd4
Suueh <matheusalmeida1948@gmail.com>
2023-03-29 12:33:12 -0300

    .

---
+++ atividade2.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program{
+
+static void Main(){
+
+
+    string nome;
+
+    Console.WriteLine("Digite seu nome");
+
+    nome = Console.ReadLine();
+
+    Console.WriteLine("Seja bem vindo {0}", nome);
+    }
+}
