cf02790804fc616ebe3148d80a747e657bb014fe
Suueh <matheusalmeida1948@gmail.com>
2023-04-18 16:39:51 -0300

    .

--- atvmetodos.cs
+++ atvmetodos.cs
@@ -3,42 +3,46 @@ class Program{
     static void Main(){
         
         Console.WriteLine("Por favor! \nfaça o cadastro a baixo");
-        int metodo = newint [cadastro()];
-        Console.Clear();
         Menu();
     }
-    static void cadastro(string nome, double altura, int idade, double peso, char sexo){
+    public static void cadastro(){
         
-        string nome = new string[];
-        double altura = new double[];
-        int idade = new int[];
-        double peso = new double[];
-        char sexo = new char[];
+        //a criaçao do vetr estrava errado estava sem os colchetes apos o ttipo da variavel e estaava sem o tamanho ddo vetor
+        string[] nome = new string[2];
+        double[] altura = new double[2];
+        int[] idade = new int[2];
+        double[] peso = new double[2];
+        char[] sexo = new char[2];
 
+        for(int i = 0; i<2; i++){
+        //a criacao do vetor estava dentro do for entao toda vvez que o for rodava e o i recbia umm incrementtto o coddigo criava novos vetores
         Console.WriteLine("Digite seu nome:");
-        nome[0] = Console.ReadLine();
+        nome[i] = Console.ReadLine();
+        // o inddice da posicciçao esttavaa sempre 0 entao naao  acumulava os cadastros
         Console.WriteLine("Digite sua altura:");
-        altura[0] = Convert.ToDouble(Console.ReadLine());
+        altura[i] = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("Digite seu idade:");
-        idade[0] = Convert.ToInt32(Console.ReadLine());
+        idade[i] = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Digite seu peso:");
-        peso[0] = Convert.ToInt32(Console.ReadLine());
+        peso[i] = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Digite seu sexo:");
-        sexo[0] = Convert.ToChar(Console.ReadLine());
-
+        sexo[i] = Convert.ToChar(Console.ReadLine());
         
+        Console.WriteLine("N do cadastro: {0}", i);
+    }
         
     }
 
-    static void Menu(){
+    public static void Menu(){
 
         inicio:
         ini:
         char escolha;
 
         Console.WriteLine("Escolha uma das opções a seguir");
-        escolha = Convert.ToChar(Console.ReadLine());
         Console.WriteLine("1)Cadastro \n2)Alterar cadastro \n3)Imprimir todos os cadastros \n4)Excluir cadastro \n5)Limpar tela \n6)Sair");
+        escolha = Convert.ToChar(Console.ReadLine());
+    
 
         switch(escolha){
 
@@ -77,8 +81,8 @@ class Program{
             goto ini;
         }
     }
-    static void excadastro(nome altura ,idade peso ,sexo ){
-        char escolha;
+    public static void excadastro(){
+        /*char escolha;
 
         Console.WriteLine("Tem certeza que deseja excluir? [S] [N]");
         escolha = Convert.ToChar(Console.ReadLine());
@@ -88,29 +92,29 @@ class Program{
             altura = 0;
             idade = 0;
             peso = 0;
-            sexo = "";
+            sexo = ' ';
 
-        }
-
-
-
-    }
-    static void AlterCadastro(){
+        }*/
 
 
 
     }
-    static void ImpriCadastro(){
+    public static void AlterCadastro(){
 
 
 
     }
-    static void Limpar(){
+    public static void ImpriCadastro(){
 
 
 
     }
-    static void Sair(){
+    public static void Limpar(){
+
+
+
+    }
+    public static void Sair(){
 
     }
     }
