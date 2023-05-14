cf6ac37f4c923c04e7da7755ade65cdde3684aa5
Suueh <matheusalmeida1948@gmail.com>
2023-05-14 15:52:49 -0300

    .

--- atvmetodo.cs
+++ atvmetodo.cs
@@ -1,71 +1,23 @@
 using System;
 class Program{
+
+        public static int tc;
+        public static string [] nome = new string[tc];
+       public static double[] altura = new double[tc];
+       public static int[] idade = new int[tc];
+       public static double[] peso = new double[tc];
+       public static char[] sexo = new char[tc];
+
     static void Main(){
-    
-        PCadastro();
-        Console.Clear();
+
         Menu();
-    }
-    public static void PCadastro(){
 
-        Console.WriteLine("Por favor! \nFaça o cadastro abaixo");
-        
-        string[] nome = new string[1];
-        double[] altura = new double[1];
-        int[] idade = new int[1];
-        double[] peso = new double[1];
-        char[] sexo = new char[1];
-        int n = 0;
-
-        for(int i = 0; i<1; i++){
-         n++;
-            
-        Console.Write("Digite seu nome:");
-        nome[i] = Console.ReadLine();
-        Console.Write("Digite sua altura:");
-        altura[i] = Convert.ToDouble(Console.ReadLine());
-        Console.Write("Digite seu idade:");
-        idade[i] = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Digite seu peso:");
-        peso[i] = Convert.ToInt32(Console.ReadLine());
-        fm:
-        Console.Write("Digite seu sexo:");
-        sexo[i] = Convert.ToChar(Console.ReadLine());
-        
-       switch(sexo[i]){
-
-        case 'm':
-        break;
-        case 'M':
-        break;
-        case 'f':
-        break;
-        case 'F':
-        break;
-        default:
-        Console.WriteLine("Você não escolheu nem 'f' nem 'm'  \nPorfavor escolha uma das opções acima");
-        goto fm;
-
-        
-       }
-       
-    }
-       
     }
     public static void Cadastros(){
 
         Console.Clear();
-
-        int tc;
-
         Console.Write("Quantos cadastros você deseja fazer:");
         tc = Convert.ToInt32(Console.ReadLine());
-
-        string[] nome = new string[tc];
-        double[] altura = new double[tc];
-        int[] idade = new int[tc];
-        double[] peso = new double[tc];
-        char[] sexo = new char[tc];
 
         for(int i = 0; i<tc; i++){
         Console.Write("Digite seu nome:");
@@ -113,7 +65,7 @@ class Program{
         Console.WriteLine("Bem Vindo!");
         inicio:
         ini:
-        Console.WriteLine("Você pode escolher uma das opções a seguir: \n1)Cadastro \n2)Alterar um cadastro \n3)Imprimir todos os cadastros \n4)Excluir cadastro \n5)Limpar tela \n6)Sair");
+        Console.WriteLine("Você pode escolher uma das opções a seguir: \n1)Cadastro \n2)Alterar um cadastro \n3)Imprimir um dos cadastros \n4)Excluir cadastro \n5)Limpar tela \n6)Sair");
         escolha = Convert.ToInt32(Console.ReadLine());
     
 
@@ -173,16 +125,62 @@ class Program{
     }
     public static void AlterCadastro(){
 
+        int nc;
 
+        Console.WriteLine("Qual cadastro você deseja alterar?");
+        nc = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite seu nome:");
+        nome[nc] = Console.ReadLine();
+        Console.Write("Digite sua altura:");
+        altura[nc] = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite sua idade:");
+        idade[nc] = Convert.ToInt32(Console.ReadLine());
+       Console.Write("Digite seu peso:");
+        peso[nc] = Convert.ToInt32(Console.ReadLine());
+        fm:
+        Console.Write("Digite seu sexo:");
+        sexo[nc] = Convert.ToChar(Console.ReadLine());
+        
+       switch(sexo[nc]){
+
+        case 'm':
+        Console.WriteLine("N do Cadastro alterado: {0}", nc);
+        break;
+        case 'M':
+        Console.WriteLine("N do Cadastro alterado: {0}", nc);
+        break;
+        case 'f':
+        Console.WriteLine("N do Cadastro alterado: {0}", nc);
+        break;
+        case 'F':
+        Console.WriteLine("N do Cadastro alterado: {0}", nc);
+        break;
+        default:
+        Console.WriteLine("Você não escolheu nem 'f' nem 'm'  \nPorfavor escolha uma das opções acima");
+        goto fm;
+
+       }
+       
+        Console.WriteLine("Cadastro alterado com sucesso!");
 
     }
     public static void ImpriCadastro(){
+        int nc;
 
+        Console.WriteLine("Qual cadastro você deseja Imprimir?");
+        nc = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Nome: {0}", nome[nc]);
+        Console.WriteLine("Altura: {0}", altura[nc]);
+        Console.WriteLine("Idade: {0}", idade[nc]);
+        Console.WriteLine("Peso: {0}", peso[nc]);
+        Console.WriteLine("Sexualidade: {0}", sexo[nc]);
 
     }
     public static void Limpar(){
 
-
+        Console.Clear();
 
     }
     public static void Sair(){
